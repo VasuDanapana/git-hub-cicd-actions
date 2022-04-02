8a6afbd4654d2650bb6327fe6e0dc1bdf30a38b8
VasuDanapana <vasu.java.techie@gmail.com>
2022-04-02 16:56:25 +0530

    Controller updated........

--- src/main/java/com/jfsd/GitHubCicdActionsApplication.java
+++ src/main/java/com/jfsd/GitHubCicdActionsApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GitHubCicdActionsApplication {
 	
-	@GetMapping("/")
+	@GetMapping("/getMessage")
 	public String message() {
 		return "Welcome to GIT Hub CICD Actions";
 	}
